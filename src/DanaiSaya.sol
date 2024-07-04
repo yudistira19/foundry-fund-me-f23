e906b56f668675b2f29bab14fa8993a9cd646583
Yudistira Adi Wahyudi R <yudistira19@gmail.com>
2024-07-04 12:01:06 +0800

    modified:   src/DanaiSaya.sol

--- src/DanaiSaya.sol
+++ src/DanaiSaya.sol
@@ -4,7 +4,7 @@ pragma solidity ^0.8.18;
 import {AggregatorV3Interface} from "@chainlink/contracts/src/v0.8/shared/interfaces/AggregatorV3Interface.sol";
 import {KonversiHarga} from "./KonversiHarga.sol";
 
-error BukanPemilik();
+error DanaiSaya__BukanPemilik();
 
 contract DanaiSaya {
     using KonversiHarga for uint256;
